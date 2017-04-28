4d8f4d6ac78cc8ca528ce2e45d8edbb45b7071df
Johan Kladder <j.kladder@st.hanze.nl>
2017-04-28 19:10:13 +0200

    + Initialisation UI

---
+++ app/Resources/views/base.html.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Welcome</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+        <link rel="stylesheet" href="<?php echo $view['assets']->getUrl('assets/vendor/bootstrap/dist/css/bootstrap.min.css') ?>">
+        <script src="<?php echo $view['assets']->getUrl('assets/vendor/jquery/dist/jquery.min.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('assets/vendor/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+      </head>
